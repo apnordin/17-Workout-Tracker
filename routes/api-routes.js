a6bfc4416be871b801aaa90d54e9667f1c4070ff
Alex Nordin <apnordin@gmail.com>
2021-01-15 10:38:29 -0500

    created empty api route for all routes found in api.js in public. completed post route for /workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
 const Workout = require ("../models/workout.js");
 
+router.get("/api/workouts", (req, res) => {
+
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+
+});
+
 router.post("/api/workouts", (req, res) => {
     Workout.create({})
     .then(dbWorkout => {
@@ -10,5 +18,11 @@ router.post("/api/workouts", (req, res) => {
         res.status(400).json(err);
     });
 });
+
+router.get("/api/workouts/range", (req, res) => {
+
+});
+
+
 
 module.exports = router;
