dadf7b03a9c772e048c1f46e6e57d11993b47804
Alex Nordin <apnordin@gmail.com>
2021-01-15 11:10:34 -0500

    Get last workout and add exercise are working in API

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,36 @@
 const router = require("express").Router();
 const Workout = require ("../models/workout.js");
 
+// Get the last workout
 router.get("/api/workouts", (req, res) => {
-
+    Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
 });
 
+// Add an exercise
 router.put("/api/workouts/:id", (req, res) => {
-
+    Workout.update(
+        {
+            _id: req.params.id
+        },
+        {
+            $push: {exercises: req.body},
+        },
+        { new: true }
+    ).then (dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
 });
 
+// Create new workouts
 router.post("/api/workouts", (req, res) => {
     Workout.create({})
     .then(dbWorkout => {
@@ -19,6 +41,7 @@ router.post("/api/workouts", (req, res) => {
     });
 });
 
+// Get workouts in range (?)
 router.get("/api/workouts/range", (req, res) => {
 
 });
